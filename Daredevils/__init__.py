e24f54d972aaa7c13cb54f57180caba7da03ef44
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2023-01-01 19:55:04 +0530

    report convert module

---
+++ Daredevils/__init__.py
@@ -0,0 +1,8 @@
+from difflib import SequenceMatcher
+def compare_report(file_1,file_2):
+    with open(file_1) as file1, open(file_2) as file2:
+        file1_data = file1.read()
+        file2_data = file2.read()
+        similarity = SequenceMatcher(None, file1_data, file2_data).ratio()
+        print(similarity)
+        print(f"The contents are {similarity*100}% common")
